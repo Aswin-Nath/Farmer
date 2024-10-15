34dd56293fb8cc36dff4b26ff280dd5583d3facc
Balaji-pv <115524041+Balaji-pv@users.noreply.github.com>
2024-10-16 00:01:47 +0530

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>HI</Text>
+      <Text>okhgjhgcv</Text>
       <StatusBar style="auto" />
     </View>
   );
