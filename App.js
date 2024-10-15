50a849370a84e4cc941df41a92c1120584195ddd
Balaji-pv <balajipugazhendi@gmail.com>
2024-10-16 00:00:00 +0530

    Value

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>BALALLALALA</Text>
+      <Text>HI</Text>
       <StatusBar style="auto" />
     </View>
   );
